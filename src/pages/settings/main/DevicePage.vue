392f7755c07750d51c0f362c09541d6c93feb7f3
ej.noh <coexist0000@gmail.com>
2025-05-01 21:04:24 +0900

    [PUB] 설정 - 단말기관리

--- src/pages/settings/main/DevicePage.vue
+++ src/pages/settings/main/DevicePage.vue
@@ -1,40 +1,44 @@
 <template>
-  <section class="contents-c-body">
-    <div class="titleBox">
-      <div class="text-title lg">단말기</div>
-      <div class="titleBox__btns">
+  <div class="settings-mid">
+    <div class="title-box">
+      <div class="title">단말기</div>
+      <div class="btns">
         <a href="javascript:;" class="btn-sm btn-ghost">초기화</a>
         <a href="javascript:;" class="btn-sm btn-basic">등록/수정</a>
       </div>
     </div>
-    <div class="formBox">
-      <ol>
-        <dt>전화번호</dt>
-        <dd>
-          <input type="text" placeholder="010-111-2222" class="w140" />
-        </dd>
-        <dt>사원ID</dt>
-        <dd>
-          <input type="text" placeholder="Test01" class="w140" />
-        </dd>
-        <dt>핸드폰유무</dt>
-        <dd>
-          <select class="w140">
-            <option>선택</option>
-          </select>
-        </dd>
-      </ol>
-    </div>
-    
-  </section>
+    <form class="form-box">
+      <div class="form-mix">
+        <label>전화번호</label>
+        <input type="text" placeholder="010-111-2222" class="w150px" />
+      </div>
+      <div class="form-mix">
+        <label>사원ID</label>
+        <input type="text" placeholder="Test01" class="w150px" />
+      </div>
+      <div class="form-mix">
+        <label>핸드폰유무</label>
+        <select class="w150px">
+          <option>선택</option>
+        </select>
+      </div>
+    </form>
+  </div>
 </template>
 
 <script setup>
-import { defineProps, defineEmits, ref, onMounted, watch, computed, reactive } from 'vue'
-import { global } from 'assets/js/publish/global'
-
+  import {
+    defineProps,
+    defineEmits,
+    ref,
+    onMounted,
+    watch,
+    computed,
+    reactive,
+  } from 'vue';
+  import { global } from 'assets/js/publish/global';
 </script>
 
 <style lang="scss" scoped>
-  @import "/src/assets/css/set.scss";
+  @import '/src/assets/css/set.scss';
 </style>
