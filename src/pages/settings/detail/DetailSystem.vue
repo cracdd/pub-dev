e67c299dda11ce200ea0099561d83306013d48ad
ej.noh <coexist0000@gmail.com>
2025-05-03 15:28:28 +0900

    시스템설정

--- src/pages/settings/detail/DetailSystem.vue
+++ src/pages/settings/detail/DetailSystem.vue
@@ -1,11 +1,341 @@
 <template>
-  <div>
+  <section class="card-top">
+    <div class="tbl-detail-box">
+      <div class="tbl-detail-title">
+        조회 설정
+        <div class="tbl-btns">
+          <a class="btn-md btn-secondary">조회</a>
+        </div>
+      </div>
+      <table class="tbl-detail">
+        <colgroup>
+          <col style="" />
+          <col style="width: 33%" />
+          <col style="" />
+          <col style="width: 33%" />
+          <col style="" />
+          <col style="" />
+        </colgroup>
+        <tr>
+          <th>코드북 코드</th>
+          <td><input type="text" placeholder="" class="w140px" /></td>
+          <th>사용여부</th>
+          <td>
+            <select class="w140px">
+              <option>사용중</option>
+              <option></option>
+            </select>
+          </td>
+          <th></th>
+          <td></td>
+        </tr>
+      </table>
+    </div>
+  </section>
 
-  </div>
+  <section class="card multi">
+    <!--검색결과-->
+    <div class="card__conts">
+      <q-table
+          title="검색결과"
+          style="height: 300px"
+          flat
+          separator="cell"
+          :rows="resultRows"
+          :columns="resultColumns"
+          row-key="name"
+          :selected-rows-label="getSelectedString"
+          v-model:selected="selected"
+          :hide-pagination="true"
+          :rows-per-page-options="[0]"
+          class="table-wrap type-data"
+        >
+          <template v-slot:top-right>
+            <a class="btn-sm btn-ghost">초기화</a>
+            <a class="btn-sm btn-basic ml5">수정</a>
+          </template>
+        </q-table>
+        <div class="settings-mid pd0">
+          <h5 class="mt25">코드셋 정보 등록/수정</h5>
+          <form class="form-box mt20">
+            <div class="form-mix">
+              <label>코드셋 코드</label>
+              <input type="text" placeholder="" value="AGT001" class="w100p" />
+            </div>
+            <div class="form-mix">
+              <label>코드셋 설명</label>
+              <input type="text" placeholder="코드셋 설명" value="" class="w100p" />
+            </div>
+            <div class="form-mix">
+              <label>코드셋명</label>
+              <input type="text" placeholder="" value="상담사 상태코드" class="w100p" />
+            </div>
+            <div class="form-mix">
+              <label>사용여부</label>
+              <div class="h40px pt10 w100p">
+                <label class="radio-item">
+									<input type="radio" name="radio1" checked>
+									<span>Y</span>
+								</label>
+								<label class="radio-item ml25">
+									<input type="radio" name="radio1">
+									<span>N</span>
+								</label>
+              </div>
+            </div>
+          </form>
+        </div>
+    </div>
+
+    <!--상세결과-->
+    <div class="card__conts">
+      <q-table
+          title="상세결과"
+          style="height: 300px"
+          flat
+          separator="cell"
+          :rows="detailRows"
+          :columns="detailColumns"
+          row-key="name"
+          :selected-rows-label="getSelectedString"
+          v-model:selected="selected"
+          :hide-pagination="true"
+          :rows-per-page-options="[0]"
+          class="table-wrap type-data"
+        >
+          <template v-slot:top-right>
+            <a class="btn-sm btn-ghost">초기화</a>
+            <a class="btn-sm btn-basic ml5">수정</a>
+          </template>
+      </q-table>
+      <h5 class="mt25">코드셋 정보 등록/수정</h5>
+
+      <div class="card__conts__codeInfo">
+        <div class="settings-mid pd0">
+          <form class="form-box mt20">
+            <div class="form-mix">
+              <label>코드</label>
+              <input type="text" placeholder="" value="01" class="w100p" />
+            </div>
+            <div class="form-mix">
+              <label>코드 설명</label>
+              <input type="text" placeholder="코드 설명" value="" class="w100p" />
+            </div>
+            <div class="form-mix">
+              <label>기타1</label>
+              <input type="text" placeholder="" value="" class="w100p" />
+            </div>
+            <div class="form-mix">
+              <label>기타3</label>
+              <input type="text" placeholder="" value="" class="w100p" />
+            </div>
+            <div class="form-mix">
+              <label>기타5</label>
+              <input type="text" placeholder="" value="" class="w100p" />
+            </div>
+          </form>
+        </div>
+        <div class="settings-mid pd0">
+          <form class="form-box mt20">
+          <div class="form-mix">
+            <label>코드명</label>
+            <input type="text" placeholder="" value="" class="w100p" />
+          </div>
+          <div class="form-mix">
+            <label>사용여부</label>
+            <div class="h40px pt10 w100p">
+              <label class="radio-item">
+                <input type="radio" name="radio1" checked>
+                <span>Y</span>
+              </label>
+              <label class="radio-item ml25">
+                <input type="radio" name="radio1">
+                <span>N</span>
+              </label>
+            </div>
+          </div>
+          <div class="form-mix">
+            <label>기타2</label>
+            <input type="text" placeholder="" value="" class="w100p" />
+          </div>
+          <div class="form-mix">
+            <label>기타4</label>
+            <input type="text" placeholder="" value="" class="w100p" />
+          </div>
+          <div class="form-mix">
+            <label>기타6</label>
+            <input type="text" placeholder="" value="" class="w100p" />
+          </div>
+          </form>
+        </div>
+      </div>
+
+    </div>
+  </section>
+  
 </template>
 
 <script setup>
 import { defineProps, defineEmits, ref, onMounted, watch } from 'vue'
 import { global } from 'assets/js/publish/global'
 
+const resultColumns = [
+    {
+      name: 'code',
+      align: 'center',
+      label: '코드셋 코드',
+      field: 'code',
+    },
+    {
+      name: 'name',
+      align: 'center',
+      label: '코드셋명',
+      field: 'name',
+    },
+    {
+      name: 'explanation',
+      align: 'center',
+      label: '설명',
+      field: 'explanation',
+      style: 'width:40%'
+    },
+    {
+      name: 'use',
+      align: 'center',
+      label: '사용여부',
+      field: 'use',
+    },
+  ];
+
+  const resultRows = [
+    {
+      code:'AGT001',
+      name:'상담사 상태코드',
+      explanation:'',
+      use:'사용중',
+    },
+    {
+      code:'AGT001',
+      name:'상담사 상태코드',
+      explanation:'',
+      use:'사용중',
+    },
+    {
+      code:'AGT001',
+      name:'상담사 상태코드',
+      explanation:'',
+      use:'사용중',
+    },
+    {
+      code:'AGT001',
+      name:'상담사 상태코드',
+      explanation:'',
+      use:'사용중',
+    },
+    {
+      code:'AGT001',
+      name:'상담사 상태코드',
+      explanation:'',
+      use:'사용중',
+    },
+    {
+      code:'AGT001',
+      name:'상담사 상태코드',
+      explanation:'',
+      use:'사용중',
+    },
+  ];
+
+  const detailColumns = [
+  {
+      name: 'code',
+      align: 'center',
+      label: '코드셋 코드',
+      field: 'code',
+    },
+    {
+      name: 'name',
+      align: 'center',
+      label: '코드셋명',
+      field: 'name',
+    },
+    {
+      name: 'explanation',
+      align: 'center',
+      label: '설명',
+      field: 'explanation',
+      style: 'width:40%'
+    },
+    {
+      name: 'use',
+      align: 'center',
+      label: '사용여부',
+      field: 'use',
+    },
+  ];
+  const detailRows = [
+    {
+      code:'11',
+      name:'점심식사',
+      explanation:'',
+      use:'사용중',
+    },
+    {
+      code:'11',
+      name:'점심식사',
+      explanation:'',
+      use:'사용중',
+    },
+    {
+      code:'11',
+      name:'점심식사',
+      explanation:'',
+      use:'사용중',
+    },
+  ];
+
+  const selected = ref([]);
+
+  function getSelectedString() {
+    return '';
+  }
 </script>
+
+<style lang="scss" scoped>
+  @import "/src/assets/css/set.scss";
+
+  .card{
+    &.multi{
+      .card__conts{
+        .settings-mid{
+          .form-box{
+            padding:0;
+            .form-mix{
+              label{
+                font-size:12px;
+                font-weight: 600;
+                color:#333;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+</style>
+
+<style>
+.table-wrap.type-data thead th{
+    position:sticky;
+    top:0;
+    z-index:2;
+}
+.q-table__card .q-table__middle{
+  border:1px solid #dedede;
+  border-top:none;
+  border-radius: 0 !important;
+}
+</style>
+
+
--- src/pages/settings/main/SystemPage.vue
+++ src/pages/settings/main/SystemPage.vue
@@ -7,18 +7,28 @@
       </div>
     </div>
     <form class="form-box">
+      <h5 class="mt25 mb25">녹취 업로드 옵션</h5>
       <div class="form-mix">
         <label class="radio-item">
           <input type="radio" name="radio1" checked />
-          <span>스위치1</span>
+          <span>
+            스위치1
+            <span class="subText">전체 업로드</span>
+          </span>
         </label>
         <label class="radio-item">
           <input type="radio" name="radio1" />
-          <span>스위치2</span>
+          <span>
+            스위치2
+            <span class="subText">매핑정보 있는 파일만 업로드</span>
+          </span>
         </label>
         <label class="radio-item">
           <input type="radio" name="radio1" />
-          <span>스위치3</span>
+          <span>
+            스위치3
+            <span class="subText">매핑정보+녹취 동의 있는 파일만 업로드</span>
+          </span>
         </label>
       </div>
     </form>
@@ -29,3 +39,20 @@
   import { defineProps, defineEmits, ref, onMounted, watch } from 'vue';
   import { global } from 'assets/js/publish/global';
 </script>
+
+<style lang="scss" scoped>
+.form-mix{
+  flex-direction: column;
+  align-items: flex-start;
+  gap:16px;
+  .radio-item{
+    width:100% !important;
+    span{
+      .subText{
+        font-size:13px; color:#999;
+        padding-left:24px;
+      }
+    }
+  }
+}
+</style>
