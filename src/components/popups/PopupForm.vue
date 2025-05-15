2bf4ca44744cab030002bd29aeb71a33037aa610
ej.noh <coexist0000@gmail.com>
2025-05-15 23:39:38 +0900

    [PUB] - SM모드일때 접수번호 팝업 수정정

--- src/components/popups/PopupForm.vue
+++ src/components/popups/PopupForm.vue
@@ -23,49 +23,71 @@
 </template>
 
 <script setup>
-import { ref, watch, onMounted } from 'vue'
+  import { ref, watch, onMounted } from 'vue';
 
-const props = defineProps({
-  visible: Boolean,
-  initData : Object
-})
+  const props = defineProps({
+    visible: Boolean,
+    initData: Object,
+  });
 
+  onMounted(() => {
+    console.log('팝업에 전달된 데이터:', props.initData);
+  });
 
-onMounted(() => {
-  console.log('팝업에 전달된 데이터:', props.initData)
-})
+  watch(
+    () => props.initData,
+    (val) => {
+      if (val) {
+        console.log('💡 새로운 팝업 데이터 감지:', val);
+      }
+    },
+    { immediate: true },
+  );
 
-watch(
-  () => props.initData,
-  (val) => {
-    if (val) {
-      console.log('💡 새로운 팝업 데이터 감지:', val)
-    }
-  },
-  { immediate: true }
-)
+  const emit = defineEmits(['update:visible', 'submit']);
 
-const emit = defineEmits(['update:visible', 'submit'])
+  // 예시 데이터 (백엔드에서 props로 받아올 수도 있음)
+  const items = ref([
+    {
+      id: 1,
+      text: '2025-010101223 홍길동(자동차인 차동차물 보여지는) 2025-03-12 11:08:44',
+    },
+    {
+      id: 2,
+      text: '2025-010101223 홍길동(자동차인 차동차물) 2025-03-12 11:08:44',
+    },
+    {
+      id: 3,
+      text: '2025-010101223 홍길동(자동차인 차동차물) 2025-03-12 11:08:44',
+    },
+    {
+      id: 4,
+      text: '2025-010101223 홍길동(자동차인 차동차물) 2025-03-12 11:08:44',
+    },
+    {
+      id: 5,
+      text: '2025-010101223 홍길동(자동차인 차동차물) 2025-03-12 11:08:44',
+    },
+    {
+      id: 6,
+      text: '2025-010101223 홍길동(자동차인 차동차물) 2025-03-12 11:08:44',
+    },
+    {
+      id: 7,
+      text: '2025-010101223 홍길동(자동차인 차동차물) 2025-03-12 11:08:44',
+    },
+  ]);
 
-// 예시 데이터 (백엔드에서 props로 받아올 수도 있음)
-const items = ref([
-  { id: 1, text: '2025-010101223 홍길동(자동차인 차동차물 보여지는) 2025-03-12 11:08:44' },
-  { id: 2, text: '2025-010101223 홍길동(자동차인 차동차물) 2025-03-12 11:08:44' },
-  { id: 3, text: '2025-010101223 홍길동(자동차인 차동차물) 2025-03-12 11:08:44' },
-  { id: 4, text: '2025-010101223 홍길동(자동차인 차동차물) 2025-03-12 11:08:44' },
-  { id: 5, text: '2025-010101223 홍길동(자동차인 차동차물) 2025-03-12 11:08:44' }
-])
+  const selectedId = ref(null);
 
-const selectedId = ref(null)
+  const onClose = () => {
+    emit('update:visible', false);
+  };
 
-const onClose = () => {
-  emit('update:visible', false)
-}
-
-// 항목 선택 후 즉시 전송
-const selectItem = (item) => {
-  selectedId.value = item.id
-  emit('submit', item)
-  emit('update:visible', false)
-}
+  // 항목 선택 후 즉시 전송
+  const selectItem = (item) => {
+    selectedId.value = item.id;
+    emit('submit', item);
+    emit('update:visible', false);
+  };
 </script>
